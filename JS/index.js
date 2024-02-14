7cde5962cdc618d5eec61856c4742379941e937d
Tamara <tamy.sb@hotmail.com>
2024-02-14 19:00:21 -0300

    simulador JS y CSS

--- JS/index.js
+++ JS/index.js
@@ -9,13 +9,13 @@ class Producto{
         let precioIVA = this.precio * 1.9;
         return "$" + precioIVA+  " " + "iva"
     }
-    this.precioConDESCUENTO = function(){
-        let precioDESCUENTO = this.precio * 1.9
-        if(precioDESCUENTO >= 9000){
-            return "$" + (precioDESCUENTO * 0.20) +  " " + "descuento"
+    this.precioConDescuento = function(){
+        let precioConDescuento = this.precio * 1.9
+        if(precioConDescuento >= 9000){
+            return "$" + (precioConDescuento-(precioConDescuento * 0.20)) +  " " + "20% descuento"
         }
         else{
-            return "$" + precioDESCUENTO + " " +"Sin descuento"
+            return "$" + precioConDescuento + " " +"Sin descuento"
         }
     }
 }
@@ -26,17 +26,36 @@ const productos=[
     new Producto("producto3", 1, 10000),
     new Producto("producto4", 1, 2000)
 ]
+//Agregar un producto nuevo
+productos.push(new Producto("producto5", 1, 2000))
 
 for(const Producto of productos){
     console.log(Producto.precioConIVA())
-    console.log(Producto.precioConDESCUENTO())
-    
+    console.log(Producto.precioConDescuento())
 }
 
+//lista productos con IVA y descuennto
+productos.forEach
+((producto)=>console.log(producto.nombre,producto.precioConIVA(),producto.precioConDescuento()))
+
+// filtrar producto
+const filtro1 = productos.filter(item=>item.precio <= 3000)
+console.log(filtro1)
+
+//Busqueda
+const resultado= productos.find((productos)=> productos.precio <=5000)
+console.log(resultado)
+
+//Sumar carrito de compras
+const precioTotal = productos.reduce(function(acumulador,producto){
+    return acumulador + (producto.precio * 1.9)
+}, 0)
+console.log("$" + precioTotal);
+
 // Calcular el precio  en cuanto a la cantidad de productos
-// let NUMERO = parseInt(prompt("Ingrese precio iva"))
-// for(let i=0; i<10; i++){
-//     let RESULTADO = NUMERO * i
-//     console.log(NUMERO + "x" + i + "=" + RESULTADO)
-// }
+let NUMERO = parseInt(prompt("Calcular precio x cantidad. Ingrese precio iva"))
+for(let i=0; i<10; i++){
+    let RESULTADO = NUMERO * i
+    console.log(NUMERO + "x" + i + "=" + RESULTADO)
+}
 
